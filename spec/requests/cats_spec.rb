4de1e3b8c1f58c434daffab65f95a58fc3184bba
nickdsaunders <>
2021-04-02 16:04:06 -0700

    added update endpoint and spec

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -38,6 +38,11 @@ RSpec.describe "Cats", type: :request do
       expect(cat.name).to eq 'Sylvester'
       expect(cat.age).to eq 50
       expect(cat.enjoys).to eq 'trying to have a tweetie sandwitch'
+
+      cat_response = JSON.parse(response.body)
+      expect(cat_response['name']).to eq 'Sylvester'
+      expect(cat_response['age']).to eq 50
+      expect(cat_response['enjoys']).to eq 'trying to have a tweetie sandwitch'
     end 
 
     it "cannot create a new cat without a name" do
@@ -54,6 +59,7 @@ RSpec.describe "Cats", type: :request do
       expect(error_response['name']).to include "can't be blank"
       expect(response).to have_http_status(422)
     end
+
     it "cannot create a new cat without a age" do
       cat_params = {
         cat: {
@@ -68,6 +74,7 @@ RSpec.describe "Cats", type: :request do
       expect(error_response['age']).to include "can't be blank"
       expect(response).to have_http_status(422)
     end
+
     it "cannot create a new cat without an enjoys description" do
       cat_params = {
         cat: {
@@ -83,6 +90,34 @@ RSpec.describe "Cats", type: :request do
       expect(response).to have_http_status(422)
     end
   end
+
+
+
+  describe "PUT /cats" do
+    it "updates a cat" do
+      #arrange - create a cat in the db
+      cat = Cat.create(name: 'Mittens', age: 1, enjoys: 'long walks on the beach')
+      
+      #arrange - the request that params are trying to edit on the created cat
+      update_cat_params = {
+        cat: {
+          name: 'Sylvester',
+          age: 50,
+          enjoys: "trying to have a tweetie sandwitch"
+        }
+      }
+
+      # act - make PUT request with cat id
+      put "/cats/#{cat.id}", params: update_cat_params
+
+      # assert
+      update_cat_response = JSON.parse(response.body)
+      expect(update_cat_response['name']).to eq 'Sylvester'
+      expect(update_cat_response['age']).to eq 50
+      expect(update_cat_response['enjoys']).to eq 'trying to have a tweetie sandwitch'
+    end 
+  end
+
 end
 
 
